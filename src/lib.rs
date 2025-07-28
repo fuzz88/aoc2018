d76aba8547b872d49589799d43f7e2e6b8c9a094
fuzz88 <ivan@oschepkov.ru>
2025-07-28 13:56:48 +0800

    init repo with useful stuff

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! # Advent of Code 2018, Rust, structured and *somehow* optimized.
+//!
+//! [![badge]][link]
+//!
+//! [badge]: https://img.shields.io/badge/github-blue?style=for-the-badge&logo=github&labelColor=grey
+//! [link]:  https://github.com/fuzz88/aoc2018
