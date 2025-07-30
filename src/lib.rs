6698d29759c25b924f523c4039afc3cc2c1e4adb
fuzz88 <ivan@oschepkov.ru>
2025-07-30 22:28:04 +0800

    add integer iterator for &str

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@
 //! [badge]: https://img.shields.io/badge/github-blue?style=for-the-badge&logo=github&labelColor=grey
 //! [link]:  https://github.com/fuzz88/aoc2018
 
+pub mod utils {
+    pub mod parse;
+}
+
 pub mod days {
     pub mod day01;
     pub mod day02;
---
+++ src/utils/parse.rs
@@ -0,0 +1,79 @@
+//! Extracts integers from the ascii string.
+//!
+//! Not the fastest solution, but convenient and mine. :-]
+
+use std::marker::PhantomData;
+use std::str::FromStr;
+
+pub struct ParsingSigned<'a, T> {
+    data: &'a str,
+    cursor: usize,
+    phantom: PhantomData<T>,
+}
+
+impl<T: FromStr> Iterator for ParsingSigned<'_, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let bytes = self.data.as_bytes();
+        let mut cursor = &mut self.cursor;
+
+        if *cursor == bytes.len() {
+            return None;
+        }
+
+        let begin = loop {
+            let byte = bytes[*cursor];
+
+            if byte.wrapping_sub(b'0') < 10 {
+                if *cursor > 0 && bytes[*cursor - 1] == b'-' {
+                    break *cursor - 1;
+                }
+                break *cursor;
+            }
+            *cursor += 1;
+
+            if *cursor == bytes.len() {
+                return None;
+            }
+        };
+
+        let end = loop {
+            if *cursor == bytes.len() - 1 {
+                break *cursor + 1;
+            }
+            if bytes[*cursor].wrapping_sub(b'0') < 10 {
+                *cursor += 1;
+            } else {
+                break *cursor;
+            }
+        };
+
+        *cursor += 1;
+
+        self.data[begin..end].parse().ok()
+    }
+}
+
+pub trait ParseOps {
+    fn iter_signed<T: FromStr>(&self) -> ParsingSigned<'_, T>;
+}
+
+impl ParseOps for &str {
+    fn iter_signed<T: FromStr>(&self) -> ParsingSigned<'_, T> {
+        ParsingSigned { data: self, cursor: 0, phantom: PhantomData }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_string() {
+        let s = String::from("123 blabla :-1-4 12 13 5 -1234565 1234 19");
+        let numbers: Vec<i32> = s.as_str().iter_signed().collect();
+
+        assert_eq!(numbers.len(), 9);
+    }
+}
