08541b26bae9cc759fcac4a091af64b8a05dbb88
fuzz88 <ivan@oschepkov.ru>
2025-07-30 11:25:35 +0800

    prefix is unnecessary in a test's name

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,11 +1,12 @@
-//! --- Day 1: Chronal Calibration ---
-//!
+//! ## --- Day 1: Chronal Calibration ---
+//! Seems simple.
 //!
 
 pub fn parse(input: &str) -> Vec<i32> {
     vec![]
 }
 
+/// Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
 pub fn part1(input: &[i32]) -> i32 {
     0
 }
@@ -19,7 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sample() {
+    fn sample1() {
         let sample = "+1\n-2\n+3\n+1";
         let input_data = parse(sample);
         assert_eq!(input_data.len(), 0);
