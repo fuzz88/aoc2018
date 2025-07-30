6bb0c27420cc95033005f17dd8e75a790720b67d
fuzz88 <ivan@oschepkov.ru>
2025-07-30 22:41:28 +0800

    add modules descriptions

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,12 @@
 //! [badge]: https://img.shields.io/badge/github-blue?style=for-the-badge&logo=github&labelColor=grey
 //! [link]:  https://github.com/fuzz88/aoc2018
 
+#[doc = "Gadgets for common tasks."]
 pub mod utils {
     pub mod parse;
 }
 
+#[doc = "Solutions, actually."]
 pub mod days {
     pub mod day01;
     pub mod day02;
