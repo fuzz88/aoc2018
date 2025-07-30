2e86d3c1b7f94faeb2202519e6164aaf7bbb0c3f
fuzz88 <ivan@oschepkov.ru>
2025-07-30 11:25:54 +0800

    input.txt file path

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-// use std::env;
+use std::env;
 // use std::fs;
 // use std::time;
-// use std::path::Path;
 use aoc2018::*;
+use std::path::Path;
 
 fn main() {
     println!("--- Advent of Code 2018 ---");
 
-    println!("part1: {:?}", days::day01::parse(&String::from("123123")));
+    if let Some(day) = env::args().nth(1) {
+        let input_file = Path::new("inputs").join(day).join("input").with_extension("txt");
+        println!("{}", input_file.display());
+        println!("run all year");
+    }
 }
