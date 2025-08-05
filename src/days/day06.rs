902ec6c69ff69e7b1a9d31960289dfc9574bfd09
fuzz88 <ivan@oschepkov.ru>
2025-08-05 23:37:27 +0800

    [day06] reading input

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,22 +1,53 @@
 //! ## --- Day 6: Chronal Coordinates ---
 //!
 
-pub fn parse(input: &str) -> Vec<i32> {
-    vec![]
+use crate::utils::parse::*;
+
+/// (0, 0) at top left.
+pub struct Point {
+    x: u32,
+    y: u32,
 }
 
-pub fn part1(input: &[i32]) -> i32 {
+impl From<&str> for Point {
+    fn from(value: &str) -> Self {
+        let mut iter = value.iter_unsigned();
+
+        let x = iter.next().unwrap();
+        let y = iter.next().unwrap();
+
+        Point { x, y }
+    }
+}
+
+pub fn parse(input: &str) -> Vec<Point> {
+    input.lines().map(Point::from).collect()
+}
+
+pub fn part1(input: &[Point]) -> usize {
     0
 }
 
-pub fn part2(input: &[i32]) -> i32 {
+pub fn part2(input: &[Point]) -> usize {
     0
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn sample1() {
-        assert!(true);
+        let input = "\
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9";
+        let input_data = parse(input);
+
+        assert_eq!(input_data.len(), 6);
+        assert_eq!(part1(&input_data), 17);
     }
 }
