2aaaea1ad73b72386658085e1d941b50497859b2
fuzz88 <ivan@oschepkov.ru>
2025-08-05 11:28:07 +0800

    [day06] init

---
+++ src/days/day06.rs
@@ -0,0 +1,22 @@
+//! ## --- Day 6: Chronal Coordinates ---
+//!
+
+pub fn parse(input: &str) -> Vec<i32> {
+    vec![]
+}
+
+pub fn part1(input: &[i32]) -> i32 {
+    0
+}
+
+pub fn part2(input: &[i32]) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn sample1() {
+        assert!(true);
+    }
+}
