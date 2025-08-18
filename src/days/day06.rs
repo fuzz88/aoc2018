a14c1194739c49f9725d8fb2d9a0f90577127708
fuzz88 <ivan@oschepkov.ru>
2025-08-18 13:02:33 +0800

    [day06] solved

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,10 +1,11 @@
 //! ## --- Day 6: Chronal Coordinates ---
 //!
+//! I do not like this solution. TBD: why?
 
 use crate::utils::parse::*;
 use std::collections::{HashMap, HashSet};
 
-/// (0, 0) at top left.
+/// Point(0, 0) is the top left.
 #[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
 pub struct Point {
     x: i32,
@@ -12,12 +13,12 @@ pub struct Point {
 }
 
 impl Point {
-    fn manhattan_distance(&self, other: &Point) -> u32 {
+    fn manhattan_distance(self, other: Point) -> u32 {
         self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
     }
 }
 
-/// Global bounding box contains (bounds) all the points.
+/// Minimal bounding box that contains all the points.
 #[derive(Debug)]
 pub struct BoundingBox {
     top: i32,
@@ -57,7 +58,7 @@ impl From<&[Point]> for BoundingBox {
 
 impl BoundingBox {
     /// Checks if the point is inside the `BoundingBox`.
-    fn contains(&self, point: &Point) -> bool {
+    fn contains(&self, point: Point) -> bool {
         point.x > self.left && point.x < self.right && point.y > self.top && point.y < self.bottom
     }
 }
@@ -78,14 +79,16 @@ pub fn parse(input: &str) -> Vec<Point> {
 }
 
 type Distances = HashMap<Point, HashMap<usize, u32>>;
+type DistancesClosest = HashMap<Point, usize>;
 
+/// `DFS` traverses closest area of the location given.
 fn get_area_size(
     idx: usize,
     loc: Point,
-    distances: &Distances,
+    distances_closest: &DistancesClosest,
     bounding_box: &BoundingBox,
-) -> Option<usize> {
-    println!("Calculating area for: {:?}", loc);
+) -> usize {
+    // let's collect points into area of the given location.
     let mut area = HashSet::<Point>::new();
     let mut to_process = vec![];
 
@@ -93,10 +96,10 @@ fn get_area_size(
     to_process.push(loc);
 
     let is_closest = |p: Point| {
-        println!("    Checking for closest: {:?}", p);
-        let point_distances = distances.get(&p).unwrap();
-        let current_distance = point_distances.get(&idx).unwrap();
-        point_distances.iter().all(|(i, x)| *i != idx && x > current_distance)
+        if let Some(closest) = distances_closest.get(&p) {
+            return *closest == idx;
+        }
+        false
     };
 
     while let Some(next_loc) = to_process.pop() {
@@ -106,13 +109,12 @@ fn get_area_size(
                     let nx = next_loc.x + dx;
                     let ny = next_loc.y + dy;
                     let neighbour = Point { x: nx, y: ny };
-                    println!("  Have neighbour: {:?}", neighbour);
-                    if !bounding_box.contains(&neighbour) {
-                        println!("    Not in the bounding box");
-                        return None;
+                    // going out of the bounding box tells us that area is infinite,
+                    if !bounding_box.contains(neighbour) {
+                        // so we can break early.
+                        return 0;
                     }
-                    if is_closest(neighbour) {
-                        println!("    Is closest");
+                    if !area.contains(&neighbour) && is_closest(neighbour) {
                         area.insert(neighbour);
                         to_process.push(neighbour);
                     }
@@ -121,32 +123,61 @@ fn get_area_size(
         }
     }
 
-    Some(area.len())
+    area.len()
 }
 
-pub fn part1(input: &[Point]) -> usize {
-    let bounding_box = BoundingBox::from(input);
+/// For each point in the bounding box, this gives us distances to each input
+/// location.
+fn calculate_distances(points: &[Point], bounding_box: &BoundingBox) -> Distances {
     let mut distances = Distances::new();
 
     for col in bounding_box.left..=bounding_box.right {
         for row in bounding_box.top..=bounding_box.bottom {
-            for (idx, loc) in input.iter().enumerate() {
+            for (idx, loc) in points.iter().enumerate() {
                 let current_point = Point { x: col, y: row };
                 let point_distances = distances.entry(current_point).or_default();
-                point_distances.insert(idx, loc.manhattan_distance(&current_point));
+                point_distances.insert(idx, loc.manhattan_distance(current_point));
             }
         }
     }
 
-    for (idx, loc) in input.iter().enumerate() {
-        println!("{}: {:?}", idx, get_area_size(idx, *loc, &distances, &bounding_box));
-    }
-
-    0
+    distances
 }
 
+/// What is the size of the largest area that isn't infinite?
+pub fn part1(input: &[Point]) -> usize {
+    let bounding_box = BoundingBox::from(input);
+    let distances = calculate_distances(input, &bounding_box);
+
+    let mut distances_closest = DistancesClosest::new();
+
+    for (point, dists) in &distances {
+        for idx in 0..input.len() {
+            let current_dist = dists.get(&idx).unwrap();
+            if dists
+                .iter()
+                .all(|(i, x)| (*i != idx && x > current_dist) || (*i == idx && x == current_dist))
+            {
+                distances_closest.insert(*point, idx);
+                break;
+            }
+        }
+    }
+
+    input
+        .iter()
+        .enumerate()
+        .map(|(idx, &loc)| get_area_size(idx, loc, &distances_closest, &bounding_box))
+        .max()
+        .unwrap()
+}
+
+/// What is the size of the region containing all locations which have a total distance to all given coordinates of less than 10000?
 pub fn part2(input: &[Point]) -> usize {
-    0
+    let bounding_box = BoundingBox::from(input);
+    let distances = calculate_distances(input, &bounding_box);
+
+    distances.values().filter(|dists| dists.values().sum::<u32>() < 10000).count()
 }
 
 #[cfg(test)]
@@ -166,6 +197,12 @@ mod test {
         assert_eq!(input_data.len(), 6);
 
         assert_eq!(part1(&input_data), 17);
+
+        // part2
+        let bounding_box = BoundingBox::from(input_data.as_slice());
+        let distances = calculate_distances(&input_data, &bounding_box);
+
+        assert_eq!(distances.values().filter(|dists| dists.values().sum::<u32>() < 32).count(), 16);
     }
 
     #[test]
@@ -181,10 +218,10 @@ mod test {
         assert_eq!(input_data.len(), 6);
 
         let bounding_box = BoundingBox::from(input_data.as_slice());
-        assert!(bounding_box.contains(&Point { x: 3, y: 4 }));
-        assert!(bounding_box.contains(&Point { x: 1, y: 1 }));
-        assert!(bounding_box.contains(&Point { x: 8, y: 9 }));
-        assert!(!bounding_box.contains(&Point { x: 0, y: 0 }));
-        assert!(!bounding_box.contains(&Point { x: 9, y: 9 }));
+        assert!(bounding_box.contains(Point { x: 3, y: 4 }));
+        assert!(bounding_box.contains(Point { x: 1, y: 1 }));
+        assert!(bounding_box.contains(Point { x: 8, y: 9 }));
+        assert!(!bounding_box.contains(Point { x: 0, y: 0 }));
+        assert!(!bounding_box.contains(Point { x: 9, y: 9 }));
     }
 }
