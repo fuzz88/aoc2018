a8dfcff604306494c8068187356620c3bacdb658
fuzz88 <ivan@oschepkov.ru>
2025-09-30 16:31:03 +0800

    [day11] summed-area table calculation

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -3,76 +3,44 @@
 //! Got a hint to use https://en.wikipedia.org/wiki/Summed-area_table
 //!
 
-pub fn parse(input: &str) -> u32 {
-    input.trim().parse().unwrap()
+// Holds largest power square for the size.
+pub struct MaxSquare {
+    x: usize,
+    y: usize,
+    size: usize,
+    power: i32,
 }
 
 #[inline]
-fn power_level(x: usize, y: usize, serial_number: u32) -> i32 {
-    (((x + 10) * y + serial_number as usize) * (x + 10) / 100 % 10 - 5) as i32
+fn power_level(x: usize, y: usize, serial_number: usize) -> i32 {
+    (((x + 10) * y + serial_number) * (x + 10) / 100 % 10 - 5) as i32
 }
 
-#[inline]
-fn find_largest(field: &[i32; 90000], size: usize) -> (usize, usize, i32) {
-    let mut result = (0, 0, 0);
-    let mut sum_max = i32::MIN;
+pub fn parse(input: &str) -> Vec<MaxSquare> {
+    let serial_number = input.trim().parse().unwrap();
 
-    for y in 1..=(300 - size + 1) {
-        for x in 1..=(300 - size + 1) {
-            let mut sum: i32 = 0;
-            for dy in y..y + size {
-                for dx in x..x + size {
-                    sum += field[(dy - 1) * 300 + (dx - 1)];
-                }
-            }
-            if sum > sum_max {
-                sum_max = sum;
-                result = (x, y, sum_max);
-            }
+    let mut sat = vec![0; 301 * 301];
+
+    for y in 1..301 {
+        for x in 1..301 {
+            let index = 301 * y + x;
+            // I(x, y) = i(x, y) + I(x, y - 1) + I(x - 1, y) - I(x - 1, y - 1)
+            sat[index] = power_level(x, y, serial_number) + sat[index - 1] + sat[index - 301]
+                - sat[index - 302];
         }
     }
-
-    result
 }
+
 /// What is the X,Y coordinate of the top-left fuel cell of the 3x3 square with the largest total power?
-pub fn part1(input: &u32) -> String {
-    let mut field = [0; 300 * 300];
-    let serial_number = *input;
-
-    for y in 1..=300 {
-        for x in 1..=300 {
-            field[(y - 1) * 300 + (x - 1)] = power_level(x, y, serial_number);
-        }
-    }
-
-    let largest = find_largest(&field, 3);
-
-    format!("{},{}", largest.0, largest.1)
+pub fn part1(input: &[MaxSquare]) -> String {
+    let MaxSquare { x, y, .. } = input.iter().find(|s| s.size == 3).unwrap();
+    format!("{x},{y}")
 }
 
 /// What is the X,Y,size identifier of the square with the largest total power?
-pub fn part2(input: &u32) -> String {
-    let mut x_y = String::new();
-    let mut field = [0; 300 * 300];
-    let serial_number = *input;
-
-    for y in 1..=300 {
-        for x in 1..=300 {
-            field[(y - 1) * 300 + (x - 1)] = power_level(x, y, serial_number);
-        }
-    }
-
-    let mut sum_max_total = i32::MIN;
-
-    for size in 1..=20 {
-        let sum = find_largest(&field, size);
-        if sum_max_total < sum.2 {
-            sum_max_total = sum.2;
-            x_y = format!("{},{},{}", sum.0, sum.1, size);
-        }
-    }
-
-    x_y
+pub fn part2(input: &[MaxSquare]) -> String {
+    let MaxSquare { x, y, size, .. } = input.iter().max_by_key(|s| s.power).unwrap();
+    format!("{x},{y},{size}")
 }
 
 #[cfg(test)]
@@ -83,7 +51,6 @@ mod test {
     fn sample1() {
         let input = "18\n";
         let input_data = parse(input);
-        assert_eq!(input_data, 18);
         assert_eq!(part1(&input_data), "33,45");
         assert_eq!(part2(&input_data), "90,269,16");
     }
