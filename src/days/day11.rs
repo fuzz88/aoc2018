c84d7d81290fffd46a552b2417637f3af2f96c6f
fuzz88 <ivan@oschepkov.ru>
2025-09-30 16:52:07 +0800

    [day11] speedup

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,7 +1,10 @@
 //! ## --- Day 11: Chronal Charge ---
 //!
-//! Got a hint to use https://en.wikipedia.org/wiki/Summed-area_table
+//! Got a hint to use <https://en.wikipedia.org/wiki/Summed-area_table>
 //!
+
+use std::sync::Mutex;
+use std::thread;
 
 // Holds largest power square for the size.
 pub struct MaxSquare {
@@ -11,9 +14,49 @@ pub struct MaxSquare {
     power: i32,
 }
 
+struct Shared {
+    sat: Vec<i32>,
+    mutex: Mutex<Vec<MaxSquare>>,
+}
+
 #[inline]
 fn power_level(x: usize, y: usize, serial_number: usize) -> i32 {
     (((x + 10) * y + serial_number) * (x + 10) / 100 % 10 - 5) as i32
+}
+
+fn parallel_calculate_max_squares(shared: &Shared) {
+    thread::scope(|scope| {
+        for size in 1..301 {
+            scope.spawn(move || {
+                let mut max_power = i32::MIN;
+                let mut max_x = 0;
+                let mut max_y = 0;
+
+                for y in size..301 {
+                    for x in size..301 {
+                        let index = 301 * y + x;
+                        let power = shared.sat[index]
+                            - shared.sat[index - size]
+                            - shared.sat[index - 301 * size]
+                            + shared.sat[index - 302 * size];
+
+                        if power > max_power {
+                            max_power = power;
+                            max_x = x - size + 1;
+                            max_y = y - size + 1;
+                        }
+                    }
+                }
+
+                shared.mutex.lock().unwrap().push(MaxSquare {
+                    x: max_x,
+                    y: max_y,
+                    size,
+                    power: max_power,
+                });
+            });
+        }
+    });
 }
 
 pub fn parse(input: &str) -> Vec<MaxSquare> {
@@ -29,6 +72,11 @@ pub fn parse(input: &str) -> Vec<MaxSquare> {
                 - sat[index - 302];
         }
     }
+
+    let shared = Shared { sat, mutex: Mutex::new(Vec::new()) };
+    parallel_calculate_max_squares(&shared);
+
+    shared.mutex.into_inner().unwrap()
 }
 
 /// What is the X,Y coordinate of the top-left fuel cell of the 3x3 square with the largest total power?
