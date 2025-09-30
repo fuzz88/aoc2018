26d67b9c977725307043e7ec05ff6219fde4a15a
fuzz88 <ivan@oschepkov.ru>
2025-09-30 12:39:59 +0800

    [day11] solved

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -10,7 +10,45 @@ fn power_level(x: usize, y: usize, serial_number: u32) -> i32 {
     (((x + 10) * y + serial_number as usize) * (x + 10) / 100 % 10 - 5) as i32
 }
 
+#[inline]
+fn find_largest(field: &[i32; 90000], size: usize) -> (usize, usize, i32) {
+    let mut result = (0, 0, 0);
+    let mut sum_max = i32::MIN;
+
+    for y in 1..=(300 - size + 1) {
+        for x in 1..=(300 - size + 1) {
+            let mut sum: i32 = 0;
+            for dy in y..y + size {
+                for dx in x..x + size {
+                    sum += field[(dy - 1) * 300 + (dx - 1)];
+                }
+            }
+            if sum > sum_max {
+                sum_max = sum;
+                result = (x, y, sum_max);
+            }
+        }
+    }
+
+    result
+}
+
 pub fn part1(input: &u32) -> String {
+    let mut field = [0; 300 * 300];
+    let serial_number = *input;
+
+    for y in 1..=300 {
+        for x in 1..=300 {
+            field[(y - 1) * 300 + (x - 1)] = power_level(x, y, serial_number);
+        }
+    }
+
+    let largest = find_largest(&field, 3);
+
+    format!("{},{}", largest.0, largest.1)
+}
+
+pub fn part2(input: &u32) -> String {
     let mut x_y = String::new();
     let mut field = [0; 300 * 300];
     let serial_number = *input;
@@ -21,28 +59,17 @@ pub fn part1(input: &u32) -> String {
         }
     }
 
-    let mut sum_max = i64::MIN;
+    let mut sum_max_total = i32::MIN;
 
-    for y in 1..=298 {
-        for x in 1..=298 {
-            let mut sum: i64 = 0;
-            for dy in y..=y + 2 {
-                for dx in x..=x + 2 {
-                    sum += field[(dy - 1) as usize * 300 + (dx - 1) as usize] as i64;
-                }
-            }
-            if sum > sum_max {
-                sum_max = sum;
-                x_y = format!("{},{}", x, y);
-            }
+    for size in 1..=20 {
+        let sum = find_largest(&field, size);
+        if sum_max_total < sum.2 {
+            sum_max_total = sum.2;
+            x_y = format!("{},{},{}", sum.0, sum.1, size);
         }
     }
 
     x_y
-}
-
-pub fn part2(input: &u32) -> i32 {
-    0
 }
 
 #[cfg(test)]
@@ -55,5 +82,6 @@ mod test {
         let input_data = parse(input);
         assert_eq!(input_data, 18);
         assert_eq!(part1(&input_data), "33,45");
+        assert_eq!(part2(&input_data), "90,269,16");
     }
 }
