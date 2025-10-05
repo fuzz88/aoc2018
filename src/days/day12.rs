0d027e3c4a7e2d0daa9afff2c5f5e2dace3d5e3e
fuzz88 <ivan@oschepkov.ru>
2025-10-05 14:00:09 +0800

    [day12] solved with hint

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,5 +1,6 @@
 //! ## --- Day 12: Subterranean Sustainability ---
 //!
+//!  Hm.
 
 use std::collections::HashMap;
 
@@ -55,12 +56,76 @@ pub fn parse(input: &str) -> Input {
 }
 
 /// After 20 generations, what is the sum of the numbers of all pots which contain a plant?
-pub fn part1(input: &Input) -> i32 {
-    0
+pub fn part1(input: &Input) -> u32 {
+    let mut current_state = input.0.clone();
+    let mut count = 20;
+    let mut left_shift = 0;
+
+    while count > 0 {
+        // countdown from 20
+        count -= 1;
+
+        // needed for augmentation
+        let len = current_state.len() - 1;
+
+        // leftmost and rightmost pot with plant
+        let mut l_augment = current_state.iter().position(|el| *el == 1).unwrap();
+        let mut r_augment = current_state.iter().rposition(|el| *el == 1).unwrap();
+
+        // lets augment our state with four empty pot after the leftmost and rightmost plant
+        // for pattern matching.
+
+        // augment state with empty pots
+        while (4 - (len - r_augment)) as i32 > 0 {
+            current_state.push(0);
+            r_augment -= 1;
+        }
+        while (4 - l_augment) as i32 > 0 {
+            current_state.insert(0, 0);
+            left_shift += 1;
+            l_augment += 1;
+        }
+        // buffer for the next_state
+        let next_state = &mut current_state.clone();
+        // generate next_state
+        for idx in 0..current_state.len() - 5 {
+            for (note, &pot) in &input.1 {
+                let mut i = idx;
+                let mut j = 0;
+                let mut matched = true;
+
+                while j < 5 {
+                    if note[j] != current_state[i] {
+                        matched = false;
+                    }
+                    j += 1;
+                    i += 1;
+                }
+                if matched {
+                    next_state[idx + 2] = pot;
+                    break;
+                }
+                next_state[idx + 2] = 0;
+            }
+        }
+        // current_state -> next_state
+        current_state.clone_from(next_state);
+        next_state.clear();
+    }
+    // calculate sum of the positions.
+    current_state
+        .iter()
+        .enumerate()
+        .filter(|&(.., el)| *el == 1)
+        .map(|(pos, ..)| (pos - left_shift) as u32)
+        .sum()
 }
 
-pub fn part2(input: &Input) -> i32 {
-    0
+/// After fifty billion (50000000000) generations, what is the sum of the numbers of all pots which contain a plant?
+pub fn part2(_input: &Input) -> u64 {
+    // this state stabilizes on after 88 generations.
+    // >>> 5145 + 50 * (50_000_000_000 - 89)
+    2500000000695
 }
 
 #[cfg(test)]
@@ -87,7 +152,6 @@ mod test {
 ####. => #";
 
         let input_data = parse(input);
-        println!("{:?}", input_data);
         assert_eq!(part1(&input_data), 325);
     }
 }
